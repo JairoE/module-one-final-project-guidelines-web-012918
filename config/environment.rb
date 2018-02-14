d30f62857f33fa97694ca71a991be81a1370c4f1
github username <github email address>
2018-02-14 10:51:29 -0500

    added table

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,8 @@ Bundler.require
 require 'pry'
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
+old_logger = ActiveRecord::Base.logger
+ActiveRecord::Base.logger = nil
 require_all 'lib'
 require 'terminal-table'
 require 'pry'
