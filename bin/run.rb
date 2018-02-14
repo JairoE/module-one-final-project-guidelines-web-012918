af25875d44d7e9f270f110926cc3bab324f0ef86
github username <github email address>
2018-02-14 15:20:11 -0500

    working on run file

--- bin/run.rb
+++ bin/run.rb
@@ -2,7 +2,10 @@ require_relative '../config/environment'
 
 greet
 game = create_users_and_game
-winner_id = play_game(game)
-
-winner_name = User.find_by_id(winner_id).name
-puts "#{winner_name} is the wiener"
+still_playing = true
+while still_playing
+  winner_id = play_game(game)
+  winner_name = User.find_by_id(winner_id).name
+  puts "#{winner_name} is the winner"
+  puts "Would you like to play another game?"
+end
