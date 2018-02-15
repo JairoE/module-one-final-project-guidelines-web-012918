e8c82bc6f243fba2ba4819d334920bf3f7383fa5
github username <github email address>
2018-02-15 13:57:47 -0500

    fixed loop kinda

--- bin/run.rb
+++ bin/run.rb
@@ -2,9 +2,9 @@ require_relative '../config/environment'
 
 greet
 still_playing = true
-game = create_users_and_game
 
 while still_playing
+  game = create_users_and_game
   winner_id = play_game(game)
   winner_name = User.find_by_id(winner_id).name
   puts "#{winner_name} is the winner"
@@ -30,4 +30,4 @@ puts ".."
 sleep(1)
 puts "."
 sleep(1)
-puts "scrubs"
+puts "scrub"
